6e0c7954b224519fa1ee098d107d25895f8ee761
bilkish-akther <bilkishakther921@gmaile.com>
2023-05-21 03:00:33 -0700

    add extra  section

---
+++ src/pages/DifferentTypesDoll/DifferentTypesDoll.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const DifferentTypesDoll = () => {
+  return (
+    <div className="differentDoll">
+      <div data-aos="fade-up"
+     data-aos-anchor-placement="bottom-bottom">
+        <h3 className="text-center mt-4  ">
+          online store of collectible and different Type of dolls{" "}
+        </h3>{" "}
+        <br />
+        <p className="text-center mt-2  ">
+          Find your dream doll in the largest catalog of collectible dolls.{" "}
+          <br /> Discover the most exclusive limited series, the classic and
+          modern dolls, <br />
+          the most tender and realistic reborn babies, the most adorable fabric
+          dolls, <br /> all the accessories you can imagine and much more.{" "}
+        </p>
+        <br />
+      </div>
+      <div className="gap-5 mb-4  differentDoll ">
+        <img data-aos="zoom-in" src="https://i.ibb.co/mCxLzWH/1.jpg" alt="" />
+        <img data-aos="zoom-in" src="https://i.ibb.co/ts3gLWX/2.jpg" alt="" />
+        <img data-aos="zoom-in" src="https://i.ibb.co/VLJWLQ3/3.png" alt="" />
+        <img data-aos="zoom-in" src="https://i.ibb.co/bvC8Tn5/4.png" alt="" />
+        <img data-aos="zoom-in" src="https://i.ibb.co/86M4W1t/5.png" alt="" />
+        <img data-aos="zoom-in" src="https://i.ibb.co/dtF4Trw/6.jpg" alt="" />
+        <img data-aos="zoom-in" src="https://i.ibb.co/R7j0F2r/7.png" alt="" />
+        <img data-aos="zoom-in" src="https://i.ibb.co/n1KdjKx/8.png" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default DifferentTypesDoll;
